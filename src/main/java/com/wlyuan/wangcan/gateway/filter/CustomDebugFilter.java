0d0642ecbad9ec1dce0b02ce8d1715eadcf4496a
wangc <wangcan4096505@qq.com>
2019-08-13 21:21:39 +0800

    debug 过滤器

--- src/main/java/com/wlyuan/wangcan/gateway/filter/CustomDebugFilter.java
+++ src/main/java/com/wlyuan/wangcan/gateway/filter/CustomDebugFilter.java
@@ -3,12 +3,14 @@ package com.wlyuan.wangcan.gateway.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import java.io.BufferedReader;
 import java.util.Map;
+import java.util.StringJoiner;
 
-
+@Slf4j
 public class CustomDebugFilter extends ZuulFilter {
     @Override
     public String filterType() {
@@ -17,7 +19,7 @@ public class CustomDebugFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
-        return 0;
+        return 1;
     }
 
     @Override
@@ -27,25 +29,31 @@ public class CustomDebugFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        RequestContext context=RequestContext.getCurrentContext();
-        HttpServletRequest request= context.getRequest();
-        request.getMethod();
-        request.getRequestURI();
-        String method = request.getParameter("method");
-        String body = request.getParameter("content");
-      Map<String, String[]>  requestParams= request.getParameterMap();
-        Map<String, List<String>>  params=context.getRequestQueryParams();
-        Map<String, String>  zuulHeaders =context.getZuulRequestHeaders();
-        if(params!=null) {
-            for (Map.Entry l : params.entrySet()) {
-                System.out.println(l.getValue());
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        StringBuffer body = new StringBuffer();
+        //请求参数
+        StringJoiner params = new StringJoiner("&");
+        Map<String, String[]> requestParams = request.getParameterMap();
+        if (requestParams != null) {
+            for (Map.Entry l : requestParams.entrySet()) {
+                String[] values = (String[]) l.getValue();
+                params.add(l.getKey() + "=" + values[0]);
             }
         }
-        if(zuulHeaders!=null) {
-            for (Map.Entry l : zuulHeaders.entrySet()) {
-                System.out.println(l.getKey() + ":" + l.getValue());
+        //处理body
+        if("post".equalsIgnoreCase(request.getMethod())){
+            try {
+                BufferedReader br = request.getReader();
+                String str = "";
+                while ((str = br.readLine()) != null) {
+                    body.append(str);
+                }
+            } catch (Exception e) {
+
             }
         }
+        log.info("url:" + request.getRequestURI()+",params:"+params.toString()+",method:" + request.getMethod() + ",body:" + body.toString());
         return true;
     }
 }
---
+++ src/main/java/com/wlyuan/wangcan/gateway/filter/CustomTokenFilter.java
@@ -0,0 +1,37 @@
+package com.wlyuan.wangcan.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.util.Map;
+import java.util.StringJoiner;
+
+@Slf4j
+public class CustomTokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String token = request.getParameter("token");
+        return true;
+    }
+}
