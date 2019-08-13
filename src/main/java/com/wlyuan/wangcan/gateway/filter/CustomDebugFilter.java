d940c986e8a0dc8cb7dcd98933d00f3559e056ea
wangc <wangcan4096505@qq.com>
2019-08-13 08:17:39 +0800

    1

--- src/main/java/com/wlyuan/wangcan/gateway/filter/CustomDebugFilter.java
+++ src/main/java/com/wlyuan/wangcan/gateway/filter/CustomDebugFilter.java
@@ -1,8 +1,12 @@
 package com.wlyuan.wangcan.gateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 
 public class CustomDebugFilter extends ZuulFilter {
@@ -23,7 +27,25 @@ public class CustomDebugFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        System.out.println("aaaa");
+        RequestContext context=RequestContext.getCurrentContext();
+        HttpServletRequest request= context.getRequest();
+        request.getMethod();
+        request.getRequestURI();
+        String method = request.getParameter("method");
+        String body = request.getParameter("content");
+      Map<String, String[]>  requestParams= request.getParameterMap();
+        Map<String, List<String>>  params=context.getRequestQueryParams();
+        Map<String, String>  zuulHeaders =context.getZuulRequestHeaders();
+        if(params!=null) {
+            for (Map.Entry l : params.entrySet()) {
+                System.out.println(l.getValue());
+            }
+        }
+        if(zuulHeaders!=null) {
+            for (Map.Entry l : zuulHeaders.entrySet()) {
+                System.out.println(l.getKey() + ":" + l.getValue());
+            }
+        }
         return true;
     }
 }
